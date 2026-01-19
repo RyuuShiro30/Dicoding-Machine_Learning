f61554a54e5bc4720c4c8560de3dc4402c3007c3
RyuuShiro30 <annoraegaprasetyo@gmail.com>
2026-01-19 19:04:20 +0700

    Unsupervised Learning Clustering

--- Unsupervised_Learning-Clustering/Case.py
+++ Unsupervised_Learning-Clustering/Case.py
@@ -20,3 +20,27 @@ plt.figure(figsize=(7, 7))
 plt.pie(df['Gender'].value_counts(), labels=['Female', 'Male'], autopct='%1.1f%%', startangle=90)
 plt.title('Gender Distribution')
 plt.show()
+
+# Mengelompokkan usia pelanggan ke dalam kategori dan menghitung jumlah pelanggan di setiap kategori
+age18_25 = df.Age[(df.Age >= 18) & (df.Age <= 25)]
+age26_35 = df.Age[(df.Age >= 26) & (df.Age <= 35)]
+age36_45 = df.Age[(df.Age >= 36) & (df.Age <= 45)]
+age46_55 = df.Age[(df.Age >= 46) & (df.Age <= 55)]
+age55above = df.Age[df.Age >= 56]
+ 
+# Menyusun data untuk plotting
+x = ["18-25", "26-35", "36-45", "46-55", "55+"]
+y = [len(age18_25.values), len(age26_35.values), len(age36_45.values), len(age46_55.values), len(age55above.values)]
+ 
+# Membuat bar chart untuk distribusi usia pelanggan
+plt.figure(figsize=(15, 6))
+plt.bar(x, y, color=['red', 'green', 'blue', 'cyan', 'yellow'])
+plt.title("Customer and Their Ages")
+plt.xlabel("Age")
+plt.ylabel("Number of Customers")
+ 
+# Menambahkan label jumlah pelanggan di atas setiap bar
+for i in range(len(x)):
+  plt.text(i, y[i], y[i], ha='center', va='bottom')
+ 
+plt.show()
